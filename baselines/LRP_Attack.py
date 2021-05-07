329090cc41d4254be5341130bdfa4190f095bdda
lbb <1650476944@qq.com>
2021-05-07 09:02:16 +0800

    LRP_Attack

--- baselines/LRP_Attack.py
+++ baselines/LRP_Attack.py
@@ -9,6 +9,7 @@ import torchvision
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
+from attack import CrossEntropyAdvLoss, LogitsAdvLoss
 
 import sys
 sys.path.append('../')
@@ -20,15 +21,12 @@ from config import MAX_DROP_BATCH as BATCH_SIZE
 from dataset import ModelNet40Attack
 from model import DGCNN, PointNetCls, PointNet2ClsSsg, PointConvDensityClsSsg
 from util.utils import str2bool, set_seed
-from attack import ClipPointsL2
+from attack import ClipPointsL2, ClipPointsLinf
 
 
 def LRP_scores(model, x, label, target):
     # stn layers
     B, Cp, Np = x.shape
-    iden = torch.from_numpy(np.array([
-        1, 0, 0, 0, 1, 0, 0, 0, 1
-    ]).astype(np.float32)).view(1, 9).repeat(x.shape[0], 1).cuda()
 
     # feat layers
     feat = model.module.feat
@@ -36,14 +34,10 @@ def LRP_scores(model, x, label, target):
     feat_len = len(feat_layers)
     feat_x = [x] + [None] * feat_len
     for i in range(feat_len):
-        if feat_layers[i] == 'SqueezeLast':
-            feat_x[i + 1] = feat_x[i].view(-1, 1024)
-        else:
-            feat_x[i + 1] = feat_layers[i].forward(feat_x[i])
+        feat_x[i + 1] = feat_layers[i].forward(feat_x[i])
 
     # classifier layers
-    cls_layers = [model.module.fc1, nn.ReLU(), nn.Sequential(model.module.fc2, model.module.bn2), nn.ReLU(),
-                  model.module.fc3]
+    cls_layers = [model.module.fc1, nn.ReLU(), model.module.fc2, nn.ReLU(), model.module.fc3]
     cls_layers = utils.toconv(cls_layers)
     cls_len = len(cls_layers)
     cls_x = [feat_x[-1]] + [None] * cls_len
@@ -51,16 +45,18 @@ def LRP_scores(model, x, label, target):
         cls_x[i + 1] = cls_layers[i].forward(cls_x[i])
 
     cls_x[-1] = F.softmax(cls_x[-1], dim=1)
+
     # ***************************************************LRP*****************************************************
     T = torch.arange(0, 40, dtype=torch.long).view(1, -1, 1).cuda()
     T1 = target.view(-1, 1, 1) == T
     T2 = label.view(-1, 1, 1) == T
+    bids = torch.arange(0, B)
 
-    cls_fR = -cls_x[-1][:, label:label + 1, :] * cls_x[-1] + cls_x[-1]*T2
+    cls_fR = -cls_x[-1][bids, label, :].unsqueeze(-2) * cls_x[-1] + cls_x[-1]*T2
 
     # cls layers
     cls_R = [None] * cls_len + [cls_x[-1] * T1]
-    # cls_R[-1] = cls_R[-1] / torch.max(cls_R[-1], dim=1)[0]
+    cls_R[-1] = cls_R[-1] / torch.max(cls_R[-1], dim=1, keepdim=True)[0]
     for i in range(0, cls_len)[::-1]:
         if isinstance(cls_layers[i], nn.ReLU):
             cls_R[i] = cls_R[i + 1]
@@ -71,8 +67,7 @@ def LRP_scores(model, x, label, target):
             if isinstance(cls_layers[i], nn.Conv1d):
                 W = rho(cls_layers[i].weight)
             else:
-                W = rho(cls_layers[i][0].weight) * ((cls_layers[i][1].weight /
-                           torch.sqrt(cls_layers[i][1].running_var + cls_layers[i][1].eps)).view(-1, 1, 1))
+                W = rho(cls_layers[i][0].weight)
 
             W = (W.data).unsqueeze(0)
             s = cls_R[i + 1] / z
@@ -85,25 +80,27 @@ def LRP_scores(model, x, label, target):
         if feat_layers[i] == 'transpose':
             feat_R[i] = feat_R[i + 1].transpose(2, 1)
         elif isinstance(feat_layers[i], nn.MaxPool1d):
+            # max_id = torch.max(feat_x[i], dim=-1)[1]
+            # feat_R[i] = torch.zeros_like(feat_x[i])
+            # batch_ids = torch.arange(0, x.shape[0]).view(-1, 1)
+            # fids = torch.arange(0, 1024).view(1, -1)
+            # feat_R[i][batch_ids, fids, max_id] = feat_R[i + 1][:, :, 0]
             distri = feat_x[i] / (torch.sum(feat_x[i], dim=-1, keepdim=True) + 1e-10)
             feat_R[i] = distri * feat_R[i+1]
         elif isinstance(feat_layers[i], nn.ReLU):
             feat_R[i] = feat_R[i + 1]
         elif i == 0:
-            lb = feat_x[i] * 0 - 1.5
-            hb = feat_x[i] * 0 + 1.5
-            z = feat_layers[i].forward(feat_x[i]) + 1e-9  # step 1 (a)
+            lb = feat_x[i] * 0 - 1.0
+            hb = feat_x[i] * 0 + 1.0
+            z = utils.newlayer(feat_layers[i], lambda p: p).forward(feat_x[i]) + 1e-9  # step 1 (a)
             z -= utils.newlayer(feat_layers[i], lambda p: p.clamp(min=0)).forward(lb)  # step 1 (b)
             z -= utils.newlayer(feat_layers[i], lambda p: p.clamp(max=0)).forward(hb)  # step 1 (c)
 
-            W = feat_layers[i][0].weight * ((feat_layers[i][1].weight /
-                                            torch.sqrt(feat_layers[i][1].running_var + feat_layers[i][1].eps)).view(-1, 1, 1))
+            W = feat_layers[i].weight
             rho_plus = lambda p: p.clamp(min=0)
-            W_plus = rho_plus(feat_layers[i][0].weight) * ((feat_layers[i][1].weight /
-                                            torch.sqrt(feat_layers[i][1].running_var + feat_layers[i][1].eps)).view(-1, 1, 1))
+            W_plus = rho_plus(feat_layers[i].weight)
             rho_minus = lambda p: p.clamp(max=0)
-            W_minus = rho_minus(feat_layers[i][0].weight) * ((feat_layers[i][1].weight /
-                                            torch.sqrt(feat_layers[i][1].running_var + feat_layers[i][1].eps)).view(-1, 1, 1))
+            W_minus = rho_minus(feat_layers[i].weight)
 
             W = (W.data).unsqueeze(0)
             W_plus = (W_plus.data).unsqueeze(0)
@@ -120,8 +117,7 @@ def LRP_scores(model, x, label, target):
             if isinstance(feat_layers[i], nn.Conv1d):
                 W = rho(feat_layers[i].weight)
             else:
-                W = rho(feat_layers[i][0].weight) * ((feat_layers[i][1].weight /
-                            torch.sqrt(feat_layers[i][1].running_var + feat_layers[i][1].eps)).view(-1, 1, 1))
+                W = rho(feat_layers[i][0].weight)
 
             W = (W.data).unsqueeze(0)
             s = feat_R[i + 1] / z
@@ -134,49 +130,59 @@ def LRP_scores(model, x, label, target):
 
 
 def AOA_Attack(model, x_adv, label, target):
-    clip_func = ClipPointsL2(budget=0.5)
+    clip_func = ClipPointsLinf(budget=0.08)
     x_ori = x_adv.clone().detach()
-    eps = 0.04
-    N = x_adv.shape[0] * x_adv.shape[1] * x_adv.shape[2]
-    alpha = 0.00016
+    R_ori = LRP_scores(model, x_ori, label, label).detach()
+    sorted_R, ids = torch.sort(R_ori.sum(-2), dim=-1, descending=True)
+    bids = torch.arange(0, x_adv.shape[0]).view(-1, 1)
+    W = torch.zeros_like(R_ori)
+    W[bids, :, ids[:, 0:100]] = 1.0
+    # W[:, :, ids[-200:]] = -1.0
+    alpha = 0.066
     iter_k = 0
-    lmd = 10
-    torch.autograd.set_detect_anomaly(True)
-    while iter_k < 200:
+    # torch.autograd.set_detect_anomaly(True)
+    g = torch.zeros_like(x_adv)
+    while iter_k < 150:
         pred, _, _ = model(x_adv)
-        lsec = torch.topk(pred, k=2, dim=-1)[1].squeeze()
-        # largest = torch.topk(pred, k=2, dim=-1)
-        # print(pred[:, label].item(), pred[:, target].item())
-        R_ori = LRP_scores(model, x_adv, label, label)
-        R_sec = LRP_scores(model, x_adv, label, lsec[1])
-        R_ori_max = R_ori.abs().max() + 1e-9
-        R_sec_max = R_sec.abs().max() + 1e-9
-        if iter_k > 1:
-            log_loss = torch.log(R_ori.abs().sum()) - torch.log(R_sec.abs().sum())
-            # log_loss = -(R_ori / R_ori_max - R_sec / R_sec_max).abs().sum()
+        #pred_at = model_at(x_adv)
+        #lsec = torch.topk(pred, k=2, dim=-1)[1].squeeze()
+        #print(pred[bids.squeeze(), label].detach().cpu().numpy(), pred[bids.squeeze(), lsec[:, 1]].detach().cpu().numpy())
+        # R_ori = LRP_scores(model, x_ori, label, label)
+        R_sec = LRP_scores(model, x_adv, label, label)
+        if iter_k > 10:
+            # log_loss = (W*R_ori).sum()
+            # log_loss = torch.log(R_ori.abs().sum()) - torch.log(R_sec.abs().sum())
+            W = 12.5*(0.08 - (x_adv-x_ori).abs())
+            log_loss = -torch.log((W*(R_ori - R_sec).abs()).sum()) - torch.log((x_adv-x_ori).abs().sum())    # + 0.01 * LogitsAdvLoss(0.0)(pred, target).mean()
         else:
-            log_loss = R_sec.sum() #  - R_sec.sum()
-        # log_loss = torch.log(R_ori.abs().sum())   #  - torch.log(R_sec.abs().sum())
-        #print(pred[:, label], pred[:, lsec[1]])
-        pred = torch.argmax(pred, dim=-1)
-        if pred != label:
-            break
+            log_loss = LogitsAdvLoss(0.0)(pred, target).mean()
+            # log_loss = R_sec.sum() #  - R_sec.sum()
         # ce_loss = nn.CrossEntropyLoss()(pred, target)
+        # log_loss = LogitsAdvLoss(0.0)(pred, target).mean()
         loss = log_loss      # + lmd * ce_loss
         loss.backward()
-        g = x_adv.grad
-        perturbation = alpha * g / (g.abs().sum() / N)
-        # with torch.no_grad():
+        # pred = torch.argmax(pred, dim=-1)
+        # if pred != label:
+        #     break
+        gt = x_adv.grad.detach()
+        nan_w = ~ torch.isnan(gt)
+        gt = gt * nan_w
+
+        norm = torch.sum(gt ** 2, dim=[1, 2]) ** 0.5
+        gt = gt / (norm[:, None, None] + 1e-12)
+        # gt = gt / (gt.abs().sum()/N)
+        # print(gt)
+        g = 0.9 * g + gt
+        perturbation = alpha * gt
         x_adv = x_adv - perturbation
-        # x_adv = x_adv.clamp(min=-1, max=1)
-        x_adv = x_ori + (x_adv-x_ori).clamp(min=-eps, max=eps)
+        # x_adv = x_ori + (x_adv-x_ori).clamp(min=-eps, max=eps)
         # x_adv = x_adv.clamp(min=-1, max=1)
         x_adv = clip_func(x_adv, x_ori)
         x_adv = (x_adv.data).requires_grad_(True)
         iter_k += 1
-        # if iter_k%40==0:
-        #     alpha *= 0.1
-        # print(loss)
+        # if iter_k == 11:
+        #     alpha = 0.816
+        # # print(loss)
 
     print("iter time is%d" %iter_k)
     return x_adv
@@ -200,10 +206,10 @@ def RMSE(xori, xadv):
     return torch.sqrt(torch.sum(x**2))
 
 def main():
-    global BATCH_SIZE, BEST_WEIGHTS
-    BATCH_SIZE = BATCH_SIZE[1024]
-    BEST_WEIGHTS = BEST_WEIGHTS['mn40'][1024]
-    cudnn.benchmark = True
+    # global BATCH_SIZE, BEST_WEIGHTS
+    # BATCH_SIZE = BATCH_SIZE[1024]
+    # BEST_WEIGHTS = BEST_WEIGHTS['mn40'][1024]
+    # cudnn.benchmark = True
 
     # build model
     model = PointNetCls(k=40, feature_transform=False)
@@ -218,19 +224,31 @@ def main():
     except RuntimeError:
         model.module.load_state_dict(state_dict)
 
+    # # attack model
+    # model_at = PointNet2ClsSsg(num_classes=40)
+    # model_at = nn.DataParallel(model_at).cuda()
+    # model_at.eval()
+    # print('Loading weight {}'.format(BEST_WEIGHTS['pointnet2']))
+    # state_dict = torch.load(BEST_WEIGHTS['pointnet2'])
+    # try:
+    #     model_at.load_state_dict(state_dict)
+    # except RuntimeError:
+    #     model_at.module.load_state_dict(state_dict)
+
     # load dataset
     test_set = ModelNet40Attack('data/attack_data.npz', num_points=1024,
                                 normalize=True)
-    test_loader = DataLoader(test_set, batch_size=1,
+    test_loader = DataLoader(test_set, batch_size=4,
                              shuffle=False, num_workers=4,
                              pin_memory=True, drop_last=False)
 
-    ti = 4
+    ti = 412
     data_iter = iter(test_loader)
     for i in range(ti):
         data = next(data_iter)
     total_num = 0
     success_num = 0
+    at_success_num = 0
     i = 0
     all_adv = []
     all_real_label = []
@@ -244,26 +262,31 @@ def main():
 
         x_pred, _, _ = model(x)
         x_pred = torch.argmax(x_pred, dim=-1)
-        if x_pred != label:
-            continue
+        # if x_pred != label:
+        #     # all_adv.append(x_adv.transpose(1, 2).contiguous().detach().cpu().numpy())
+        #     # all_real_label.append(label.detach().cpu().numpy())
+        #     # all_target_label.append(target.detach().cpu().numpy())
+        #     continue
 
-        total_num += 1
+        total_num += x.shape[0]
         x_adv = AOA_Attack(model, x, label, target)
-        # x_adv = Drop(model, x, label, target)
         pred, _, _ = model(x_adv)
         pred = torch.argmax(pred, dim=-1)
-        if pred != label:
-            success_num += 1
+        success_num += (pred != label).sum().cpu().item()
+        logits_at = model_at(x_adv)
+        pred_at = torch.argmax(logits_at, dim=-1)
+        at_success_num += (pred_at != label).sum().cpu().item()
         i += 1
-        if i % 50 == 0:
+        if i % 20 == 0:
             print("current attack success rate is", success_num / total_num)
+            print("current pointnet++ attack success rate is", at_success_num / total_num)
         all_adv.append(x_adv.transpose(1, 2).contiguous().detach().cpu().numpy())
         all_real_label.append(label.detach().cpu().numpy())
         all_target_label.append(target.detach().cpu().numpy())
-        # if i % 50 == 0:
+        # if i % 20 == 0:
         #     break
         # R0 = LRP_scores(model, x_adv, label, label)
-        # # R1 = LRP_scores(model, x, label, label)
+        # R1 = LRP_scores(model, x, label, label)
         # utils.pc_heatmap(x_adv.transpose(2, 1)[0], R0[0].sum(-2).unsqueeze(-1))
 
     attacked_data = np.concatenate(all_adv, axis=0)  # [num_data, K, 3]
@@ -288,5 +311,19 @@ def main():
 
 
 if __name__ == '__main__':
+    global BATCH_SIZE, BEST_WEIGHTS
+    BATCH_SIZE = BATCH_SIZE[1024]
+    BEST_WEIGHTS = BEST_WEIGHTS['mn40'][1024]
+    cudnn.benchmark = True
+    # attack model
+    model_at = PointNet2ClsSsg(num_classes=40)
+    model_at = nn.DataParallel(model_at).cuda()
+    model_at.eval()
+    print('Loading weight {}'.format(BEST_WEIGHTS['pointnet2']))
+    state_dict = torch.load(BEST_WEIGHTS['pointnet2'])
+    try:
+        model_at.load_state_dict(state_dict)
+    except RuntimeError:
+        model_at.module.load_state_dict(state_dict)
     main()
     print("End!!!")
